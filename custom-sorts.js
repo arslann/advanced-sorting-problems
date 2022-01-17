1dc888a341d5034c179e6739a997a0f8eceb08c2
arslann <arslansahin.1907@gmail.com>
2022-01-18 01:52:32 +0300

    complete quick sort

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,4 +1,37 @@
+function quicksort(arr) {
 
+  // Check if the input is length 1 or less
+  // If so, it's already sorted: return
+
+
+  if (arr.length <= 1) return arr;
+  // Pick a pivot
+
+  let pivot = arr[0];
+
+  let smaller = [];
+  let larger = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    let el = arr[i];
+
+    if (el < pivot) smaller.push(el);
+    else larger.push(el);
+  }
+
+  let smallerSorted = quicksort(smaller);
+  let largerSorted = quicksort(larger);
+
+  return [...smallerSorted, pivot, ...largerSorted]
+
+  // Put all values smaller than the pivot to the `left`
+  // Put all values larger than the pivot to the `right`
+
+  // Sort the left half
+  // Sort the right half
+
+  // Return the array with the left, pivot, and right values
+}
 
 function ageSort(users) {
   // Fill this in
@@ -22,4 +55,9 @@ function frequencySort(arr) {
 }
 
 module.exports = [oddEvenSort, validAnagrams, reverseBaseSort,
-                  frequencySort, ageSort];
+  frequencySort, ageSort];
+
+
+let arr = [5, 4, 10, 1, 8, 3, 6, 2, 22, 18, 0, 50, 48]
+
+console.log(quicksort(arr))
